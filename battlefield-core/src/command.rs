13a664f593f0c89729b28a213d48354f03cf1a2e
Cameron Eldridge <cameldridge@gmail.com>
2022-04-24 10:58:51 -0400

    Remove now irrelevant fromstr impl

--- battlefield-core/src/command.rs
+++ battlefield-core/src/command.rs
@@ -1,13 +1,4 @@
 use serde::Deserialize;
-use std::str::FromStr;
 
 #[derive(Deserialize, Debug)]
 pub struct Command(String);
-
-impl FromStr for Command {
-    type Err = std::convert::Infallible;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Self(s.to_owned()))
-    }
-}
